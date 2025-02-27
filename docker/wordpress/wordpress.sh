4b9d8215a946f88714b97b7ceab830b72f5f086c
JIA0010 <HIROsroutine@gmail.com>
2025-02-27 15:58:06 +0900

    add:can connect with https

--- docker/wordpress/wordpress.sh
+++ docker/wordpress/wordpress.sh
@@ -1,3 +1,31 @@
 
+mkdir /var/www/
+mkdir /var/www/html
 
+cd /var/www/html
+
+# Download wp-cli
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+# Make it executable
+chmod +x wp-cli.phar
+
+# Move it to /usr/local/bin/
+mv wp-cli.phar /usr/local/bin/wp
+
+# Download WordPress
+wp core download --allow-root
+
+# Create wp-config.php(rename wp-config-sample.php)
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+# mv /wp-config.php /var/www/html/wp-config.php # Copy wp-config.php to the right place
+
+# # Replace database settings
+sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
+sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
+sed -i "s/password_here/$DB_PASSWORD/g" /var/www/html/wp-config.php
+
+#download and install wordpress
+wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --skip-email --allow-root
+
+/usr/sbin/php-fpm7.3 -F
---
+++ docker/wordpress/wp-config.php
@@ -0,0 +1,12 @@
+
+
+define('DB_NAME', 'inception'); // データベース名
+define('DB_USER', 'user'); // データベースユーザー名
+define('DB_PASSWORD', 'inception'); // データベースパスワード
+define('DB_HOST', 'mariadb'); // データベースホスト（通常は 'localhost'）
+
+<!-- 
+define('AUTH_KEY', 'ランダムな文字列');
+define('SECURE_AUTH_KEY', 'ランダムな文字列');
+define('LOGGED_IN_KEY', 'ランダムな文字列');
+define('NONCE_KEY', 'ランダムな文字列'); -->
