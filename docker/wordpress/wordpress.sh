346f0c1cce889487e32892dd9087c09324f26b58
JIA0010 <HIROsroutine@gmail.com>
2025-03-01 18:12:03 +0900

    add sendmail and environmental variables

--- docker/wordpress/wordpress.sh
+++ docker/wordpress/wordpress.sh
@@ -56,7 +56,7 @@ fi
 #     --allow-root
 
 #download and install wordpress
-wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --skip-email --allow-root
+wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_PASSWORD --allow-root
 
 exec "$@"
 
