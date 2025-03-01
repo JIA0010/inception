981f0d55b5f5d5795043e8c57a14be48bfcd4295
JIA0010 <HIROsroutine@gmail.com>
2025-03-01 18:38:45 +0900

    一分でワードプレスがEXITする問題を解決している途中（CMD [php-fpm7.4, -F]を追加）

--- docker/mariadb/init.sql
+++ docker/mariadb/init.sql
@@ -1,19 +1,6 @@
 -- DB起動時に実行するSQL
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 
--- USE ${DB_NAME};
-
--- CREATE TABLE IF NOT EXISTS users (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     username VARCHAR(50) NOT NULL,
---     password VARCHAR(255) NOT NULL,
---     email VARCHAR(100) NOT NULL,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
-
--- INSERT INTO users (username, password, email) VALUES ('admin', 'admin_password', 'admin@example.com');
--- admin-pass
---なぜかパスワードがエラーとしてかえる
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 
--- docker/wordpress/wordpress.sh
+++ docker/wordpress/wordpress.sh
@@ -58,6 +58,7 @@ fi
 #download and install wordpress
 wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_PASSWORD --allow-root
 
+
 exec "$@"
 
 
