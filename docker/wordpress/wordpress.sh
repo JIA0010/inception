2300854a0e4feb4c282b834bf9aca56a49796b5e
JIA0010 <HIROsroutine@gmail.com>
2025-03-01 12:10:53 +0900

    fix: wordpress.sh

--- docker/wordpress/wordpress.sh
+++ docker/wordpress/wordpress.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #ベースは今回はdebian。ワードプレスはdocker hubから取得したものによると（ドキュメントを読んでもだと思うが）/var/www/htmlにインストールされる。
 
+set -e  # エラーハンドリング（エラー時に即停止）
+
 mkdir -p /var/www/html
 
 cd /var/www/html
@@ -29,11 +31,23 @@ fi
 
 
 # # Replace database settings
-sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
-sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
-sed -i "s/password_here/$DB_PASSWORD/g" /var/www/html/wp-config.php
+# sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
+# sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
+# sed -i "s/password_here/$DB_PASSWORD/g" /var/www/html/wp-config.php
+if [ -f /var/www/html/wp-config.php ]; then
+    wp config create --path=/var/www/html --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root
+fi
+# wp config create --path=/var/www/html \
+#     --dbname=$DB_NAME \
+#     --dbuser=$DB_USER \
+#     --dbpass=$DB_PASSWORD \
+#     --dbhost=$DB_HOST \
+#     --allow-root
 
 #download and install wordpress
 wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --skip-email --allow-root
 
 exec "$@"
+
+
+# docker exec -it wordpress
