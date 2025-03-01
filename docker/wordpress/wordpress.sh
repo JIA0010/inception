89ca16c1e3e4980ded7d37eff6e4aafb1684296b
JIA0010 <HIROsroutine@gmail.com>
2025-03-01 17:49:20 +0900

    solve wordpress is exited(htmlファイルをrmする。コアダウンロードできるようになる。)

--- docker/wordpress/wordpress.sh
+++ docker/wordpress/wordpress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #ベースは今回はdebian。ワードプレスはdocker hubから取得したものによると（ドキュメントを読んでもだと思うが）/var/www/htmlにインストールされる。
 
-envsubst < /tmp/wp-config.php > /var/www/html/wp-config.php
+# envsubst < /tmp/wp-config.php > /var/www/html/wp-config.php
 
 set -e  # エラーハンドリング（エラー時に即停止）
 
@@ -9,7 +9,7 @@ mkdir -p /var/www/html
 
 cd /var/www/html
 
-# Download wp-cli
+# Download wp-cli。wp-cli.phar以外を消す（wp coreはからのディレクトリでしか動かない）
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 # Make it executable
@@ -18,25 +18,22 @@ chmod 755 wp-cli.phar
 
 # Move it to /usr/local/bin/
 mv wp-cli.phar /usr/local/bin/wp
+rm -rf /var/www/html/*
 
 # Download WordPress  ここが現在問題
-if [ ! -f wp-config.php ]; then
+if [ ! -f /var/www/html/wp-config.php ]; then
     # wp core download --allow-root
     wp core download --path=/var/www/html --locale=ja --allow-root
 fi
 # wp core download --allow-root
 
 # Create wp-config.php(rename wp-config-sample.php)
-if [ -f /var/www/html/wp-config.php ]; then
-    mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-fi
+# if [ -f /var/www/html/wp-config.php ]; then
+#     mv /var/www/html/wp-config.php /var/www/html/wp-config.php
+# fi
 # mv /wp-config.php /var/www/html/wp-config.php # Copy wp-config.php to the right place
 
 
-# # Replace database settings
-# sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
-# sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
-# sed -i "s/password_here/$DB_PASSWORD/g" /var/www/html/wp-config.php
 
 #いるかわからないが、wp-cliの設定を行う
 if [ ! -f /var/www/html/wp-config.php ]; then
