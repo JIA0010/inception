0d343bca7de57cb05522f39877fd68df14d1417f
JIA0010 <HIROsroutine@gmail.com>
2025-03-01 16:06:08 +0900

    update:makefile bals add(wordpress:sh): envsubst chown root add(dockerfile-wp): gettext

--- docker/wordpress/wordpress.sh
+++ docker/wordpress/wordpress.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #ベースは今回はdebian。ワードプレスはdocker hubから取得したものによると（ドキュメントを読んでもだと思うが）/var/www/htmlにインストールされる。
 
+envsubst < /tmp/wp-config.php > /var/www/html/wp-config.php
+
 set -e  # エラーハンドリング（エラー時に即停止）
 
 mkdir -p /var/www/html
@@ -11,7 +13,8 @@ cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 # Make it executable
-chmod +x wp-cli.phar
+chown root:root wp-cli.phar
+chmod 755 wp-cli.phar
 
 # Move it to /usr/local/bin/
 mv wp-cli.phar /usr/local/bin/wp
@@ -34,9 +37,20 @@ fi
 # sed -i "s/database_name_here/$DB_NAME/g" /var/www/html/wp-config.php
 # sed -i "s/username_here/$DB_USER/g" /var/www/html/wp-config.php
 # sed -i "s/password_here/$DB_PASSWORD/g" /var/www/html/wp-config.php
-if [ -f /var/www/html/wp-config.php ]; then
-    wp config create --path=/var/www/html --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root
+
+#いるかわからないが、wp-cliの設定を行う
+if [ ! -f /var/www/html/wp-config.php ]; then
+    echo "wp-config.php が存在しません。新規作成します..."
+    wp config create --path=/var/www/html --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST" --allow-root
+else
+    echo "wp-config.php が既に存在します。設定を更新します..."
+    wp config set DB_NAME "$DB_NAME" --path=/var/www/html --allow-root
+    wp config set DB_USER "$DB_USER" --path=/var/www/html --allow-root
+    wp config set DB_PASSWORD "$DB_PASSWORD" --path=/var/www/html --allow-root
+    wp config set DB_HOST "$DB_HOST" --path=/var/www/html --allow-root
 fi
+
+
 # wp config create --path=/var/www/html \
 #     --dbname=$DB_NAME \
 #     --dbuser=$DB_USER \
@@ -51,3 +65,15 @@ exec "$@"
 
 
 # docker exec -it wordpress
+
+
+
+
+
+
+# 可能性
+# wp-cli のキャッシュフォルダの権限を修正：
+
+# mkdir -p /root/.wp-cli
+# chown -R root:root /root/.wp-cli
+# chmod -R 755 /root/.wp-cli
